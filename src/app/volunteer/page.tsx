bc16c40d8e4d379dce8fc0977ebace60db7ae581
pouyali <62963706+pouyali@users.noreply.github.com>
2026-03-14 07:04:29 -0700

    fix: remove unused useState import from volunteer page

--- src/app/volunteer/page.tsx
+++ src/app/volunteer/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { useRouter } from 'next/navigation';
 import VolunteerRequests from '@/components/volunteer/VolunteerRequests';
 
