795a70fbc91de8f815c2c8d148e5204f903e601e
pouyali <62963706+pouyali@users.noreply.github.com>
2026-03-14 06:17:14 -0700

    Fix avatar.tsx to use NextImage instead of img tag

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,43 +1,60 @@
-import * as React from "react"
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import NextImage from "next/image";
+import { cn } from "@/lib/utils";
 
-const Avatar = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
-    <div
-      ref={ref}
-      className={cn(
-        "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
-        className
-      )}
-      {...props}
+const Avatar = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
+      className
+    )}
+    {...props}
+  />
+));
+Avatar.displayName = "Avatar";
+
+interface AvatarImageProps {
+  src?: string;
+  alt?: string;
+  className?: string;
+}
+
+const AvatarImage = ({ src, alt = "", className }: AvatarImageProps) => {
+  const [error, setError] = React.useState(false);
+
+  if (!src || error) {
+    return null;
+  }
+
+  return (
+    <NextImage
+      src={src}
+      alt={alt}
+      fill
+      className={cn("object-cover", className)}
+      onError={() => setError(true)}
     />
-  )
-)
-Avatar.displayName = "Avatar"
+  );
+};
+AvatarImage.displayName = "AvatarImage";
 
-const AvatarImage = React.forwardRef<HTMLImageElement, React.ImgHTMLAttributes<HTMLImageElement>>(
-  ({ className, ...props }, ref) => (
-    <img
-      ref={ref}
-      className={cn("aspect-square h-full w-full", className)}
-      {...props}
-    />
-  )
-)
-AvatarImage.displayName = "AvatarImage"
+const AvatarFallback = React.forwardRef<
+  HTMLSpanElement,
+  React.HTMLAttributes<HTMLSpanElement>
+>(({ className, ...props }, ref) => (
+  <span
+    ref={ref}
+    className={cn(
+      "flex h-full w-full items-center justify-center rounded-full bg-gray-800 text-white text-sm font-medium select-none",
+      className
+    )}
+    {...props}
+  />
+));
+AvatarFallback.displayName = "AvatarFallback";
 
-const AvatarFallback = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
-    <div
-      ref={ref}
-      className={cn(
-        "flex h-full w-full items-center justify-center rounded-full bg-gray-100 text-gray-600 font-medium",
-        className
-      )}
-      {...props}
-    />
-  )
-)
-AvatarFallback.displayName = "AvatarFallback"
-
-export { Avatar, AvatarImage, AvatarFallback }
+export { Avatar, AvatarImage, AvatarFallback };
