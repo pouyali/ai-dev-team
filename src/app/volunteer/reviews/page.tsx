e215a37f142903b3b935dbde6177a708f6caa8fb
pouyali <62963706+pouyali@users.noreply.github.com>
2026-03-14 07:02:25 -0700

    fix: add volunteer reviews page route

---
+++ src/app/volunteer/reviews/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import VolunteerReviews from '@/components/volunteer/VolunteerReviews';
+
+export default function ReviewsPage(): JSX.Element {
+  return <VolunteerReviews />;
+}
