02fa89a907a234bc04d23ae9efd652101b1c187b
pouyali <62963706+pouyali@users.noreply.github.com>
2026-03-14 07:06:37 -0700

    feat: add volunteer notifications page in route group

---
+++ src/app/volunteer/(portal)/notifications/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import VolunteerNotifications from '@/components/volunteer/VolunteerNotifications';
+
+export default function VolunteerNotificationsPage(): JSX.Element {
+  return <VolunteerNotifications />;
+}
