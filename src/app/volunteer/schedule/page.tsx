3d1e8f504fed8dc0c67ebe906b89e36e3b6e9e19
pouyali <62963706+pouyali@users.noreply.github.com>
2026-03-14 07:02:22 -0700

    fix: add volunteer schedule page route

---
+++ src/app/volunteer/schedule/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import VolunteerSchedule from '@/components/volunteer/VolunteerSchedule';
+
+export default function SchedulePage(): JSX.Element {
+  return <VolunteerSchedule />;
+}
