12d18035b8ce6ab9b31ab29bdfea8b9d701b85f1
pouyali <62963706+pouyali@users.noreply.github.com>
2026-03-14 07:06:36 -0700

    feat: add volunteer schedule page in route group

---
+++ src/app/volunteer/(portal)/schedule/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import VolunteerSchedule from '@/components/volunteer/VolunteerSchedule';
+
+export default function VolunteerSchedulePage(): JSX.Element {
+  return <VolunteerSchedule />;
+}
